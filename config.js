9b0f3e41563ea9a4581f2f8f8e87e7bde3f9d0dc
Derrick Marr <dmarr000@yahoo.com>
2021-07-10 14:12:24 -0400

    Week 4 Assignment

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': 'UPDATE_ME',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: 'UPDATE_ME',
-        clientSecret: 'UPDATE_ME'
-    }
-}
